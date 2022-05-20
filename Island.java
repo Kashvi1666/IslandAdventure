aadd1c1eb7ea9aeda566598b0549312b8b602f95
kashvi <79034048+Kashvi1666@users.noreply.github.com>
2022-05-20 14:05:27 -0700

    Create Island.java
    created healthcheck method

--- Island.java
+++ Island.java
@@ -1,11 +1,11 @@
 
 public class Island extends Player(){
-  private int days;
+  private int numDays;
   public Island{
     
     public String wildAnimals(){
       System.out.println("oh no! wild animals are chasing you. if you have enough food, you will live.")
-      if (food < 5){
+      if (numFood < 5){
         return "...you have died, you did not have enough food for energy to run from wild animals";
         health == false; 
       } 
@@ -20,66 +20,40 @@ public class Island extends Player(){
       System.out.println("you are running low on water! play this number guessing game to get more for tonight.")
       System.out.println("you have three guesses, pick any whole number between 1 & 20"); 
       int guessNumber = (Math.random()*15)+5; 
-      //add scanner later to finish this
+      int guess = myObj.nextLine();
+      for (int i = 0; i <= 3; i++){
+	      if (guessNumber == guess){
+		      numWater += 5; 
+		      return("nice job! u guessed right, you have " + numWater + " water.");
+	      }
+	      else {
+		      System.out.println("guess again"); 
+	      } 
+      } 
+      return "sorry! u are out of guesses. u currently have " + numWater + " water."); 
     } 
     
-    public String daily(){
-	    if numDays = 20{
+    public String dayTracker(){
+	    healthCheck(); 
+	    numFood = numFood - 2; 
+	    numWater = numWater - 2; 
+	    if numDays = 20 {
 		    System.out.println(“A rescue boat has come!”);
 	    }
-	    if numDays != 20{
+	    if numDays != 20 {
 		    numDays++;
 		  }
-	    return numDays;
-    }                  
+	    return "days left: " + numDays;
+	    if (numWater <= 5) {
+		    findWater(); 
+	    } 
+    } 
 	  
-	 
-	  
-	  public String buildShelter(){
-	   if (wood >= 20){
-	   	System.out.print("You have built a shelter");
-		wood =-20;
-	   }
-		
-	    else{
-		numWater =-30;
-		numFood =-30;
-	    }	  
-	   }
-	  
-	  
-	  
-	  }
-	  
-	  
-	  
-	  
-	public String getResources(){
-		System.out.print("It is the " + numDays + "day. You have " + numFood + " resources and " + numWater + " water resources. Do you have a blanket? " + blanket + " Do you have a knife " + knife + " You have " + wood + " resources."); 
-
-	public int getNumDays(){
-		return numDays;
-	  }
-
-	public int getnumFood(){
-		return numFood;
-	}
-
-	public int getNumWater(){
-		return numWater;
-	}
-
-	public boolean getBlanket(){
-		return blanket;
-	}
-
-	public boolean getKnife(){
-		return knife;
-	}
-
-	public boolean getWood(){
-		return wood;
-	}
-
+    public String healthCheck(){
+	    if (numWater <= 0) || (numFood <= 0){
+		return "you are out of necessary resources, you died."; 
+		break;     
+	    } 	  
+    } 
   }   
 }
