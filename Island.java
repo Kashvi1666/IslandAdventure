592016dcef22341a128d73268d6eb08f318cd900
ParieKumar <89890674+ParieKumar@users.noreply.github.com>
2022-05-27 10:51:58 -0700

    Create Island.java

--- Island.java
+++ Island.java
@@ -1,19 +1,10 @@
-
 java.util.*;
 
 public class Island extends Player(){
-	
   private int numDays;
   private int dailyMethods; 
-	
   public Island{
-    public String getResources(){
-    	System.out.print("It is the " + numDays + "day. You have " + numFood + " resources and " + numWater + " water resources. Do you have a blanket? " + blanket + " Do you have a knife " + knife + " You have " + wood + " resources."); 
-
-    }
-    public int getNumDays(){
-	return numDays;
-    }
+    
     public String wildAnimals(){
       Scanner myObj = new Scanner(System.in);
       System.out.println("Shoot: ");
@@ -50,37 +41,62 @@ public class Island extends Player(){
         food = 0; 
       }  
     }
-  
+
+    public String findWater(int guess){
+      //in the dayTracker method, at the end of the day check water resources. call this method if user is low
+      System.out.println("You are running low on water! Play this number guessing game to replenish.")
+      System.out.println("You have three guesses, pick any whole number between 1 & 20 inclusive."); 
+      int guessNumber = (Math.random()*15)+5; 
+      int guess = myObj.nextLine();
+      for (int i = 0; i <= 3; i++){
+	      if (guessNumber == guess){
+		      numWater += 5; 
+		      return "Nice job! You guessed right! You have " + numWater + " water.";
+	      }
+	      else {
+		      System.out.println("Guess again."); 
+	      } 
+      } 
+      return "Sorry! You are out of guesses, you currently have " + numWater + " water."; 
+      if (numWater <= 0) || (numFood <= 0){
+		return "You ran out of resources, you died.";
+	        break; 
+	    }
+    } 
+    
     //gains wood, guess word game (ex: le_o_ is lemon)
     public String jungleHike(){ 
 	    //random array of words
 	    ArrayList<Type> guessingWords = new ArrayList<Type>();
-	    	guessingWords.add("sand");
-	    	guessingWords.add("umbrella");
-	    	guessingWords.add("papaya");
-	    	guessingWords.add("tree");
-	    	guessingWords.add("beach");
 	    	guessingWords.add("s_n_");
+	    	guessingWords.add("sand");
 	    	guessingWords.add("_m_re_la");
+	    	guessingWords.add("umbrella");
 	    	guessingWords.add("pa_ay_");
+	    	guessingWords.add("papaya");
 	    	guessingWords.add("tr_e");
+	    	guessingWords.add("tree");
 	    	guessingWords.add("b_a_h");
-	    int x = Math.random()*5;
+	    	guessingWords.add("beach");
+		
+	    	int x = (int)(Math.random() * 9);
+		x = 2*x;
+}
 	    String wordToGuess;
-	    if (x = 1){ 
-		    wordToGuess = array[1];
-	     }
-	    if (x = 2){ 
+	    if (x == 1){ 
 		    wordToGuess = array[2];
 	     }
-	    if (x = 3){ 
-		    wordToGuess = array[3];
-	     }
-	    if (x = 4){ 
+	    if (x == 3){ 
 		    wordToGuess = array[4];
 	     }
-	    if (x = 5){ 
-		    wordToGuess = array[5];
+	    if (x == 5){ 
+		    wordToGuess = array[6];
+	     }
+	    if (x == 7){ 
+		    wordToGuess = array[8];
+	     }
+	    if (x == 9){ 
+		    wordToGuess = array[10];
 	     }
 	    //random selection 
 	    //random index remove letters (take lenth of word, randomly selected index for 3 letters and then replace with _)
@@ -180,30 +196,6 @@ public class Island extends Player(){
 	    }
     } 
 		    
-    public String findWater(int guess){
-      //in the dayTracker method, at the end of the day check water resources. call this method if user is low
-      System.out.println("You are running low on water! Play this number guessing game to replenish.")
-      System.out.println("You have three guesses, pick any whole number between 1 & 20 inclusive."); 
-      int guessNumber = (Math.random()*15)+5; 
-      int guess = myObj.nextLine();
-      for (int i = 0; i <= 3; i++){
-	      if (guessNumber == guess){
-		      numWater += 5; 
-		      return "Nice job! You guessed right! You have " + numWater + " water.";
-	      }
-	      else {
-		      System.out.println("Guess again."); 
-	      } 
-      } 
-      return "Sorry! You are out of guesses, you currently have " + numWater + " water."; 
-    }
-		    
-    public String findFood(int guess){
-	    System.out.println("You are running low on food! Play this number guessing game to replenish.");
-	    System.out.println("You have three guesses, pick any letter between a-k (abcdefghijk)");
-	    String alphabet[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}; 
-    }
-		    
     //randomly decides obstacles for the day 
     public int chooseObstacles(){
 	    
@@ -211,13 +203,22 @@ public class Island extends Player(){
     
     //checks if game continues, updates day, starts new day 
     public String dayTracker(){
+	    
+	    //checks life before continuing 
+	    healthCheck(); 
+	  
+	    if (numWater <= 0) || (numFood <= 0){
+		return "you ran out of resources, you died.";
+	        break; 
+	    }
 	    //updates resources 
 	    numFood = numFood - 2; 
 	    numWater = numWater - 2; 
 	    
-	    //choose two methods to run on a day 
+	    //choose methods to run on a day 
 	    int obstacleType = ; 
-
+	    
+	    
 	    
 	    //update days until 10 are completed 
 	    if numDays = 10 {
@@ -233,15 +234,5 @@ public class Island extends Player(){
 		    findWater(); 
 	    } 		        		      
     }  
-				       
-    public boolean healthCheck(){
-	   //checks life before continuing 
-	    if (numWater <= 0) || (numFood <= 0){
-		return false; 
-		break; 
-	    } 
-	    return true; 
-    } 
-				       
   }   
 }
