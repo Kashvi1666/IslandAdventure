d3681a7ef53c75b4cc0b50d9096042182a48aa7e
kashvi <79034048+Kashvi1666@users.noreply.github.com>
2022-05-27 11:15:10 -0700

    Update Island.java

--- Island.java
+++ Island.java
@@ -187,34 +187,25 @@ public class Island extends Player(){
 	    }
     } 
 		    
-    public String findWater(int guess){
+    public String findResource(int guess){
       //in the dayTracker method, at the end of the day check water resources. call this method if user is low
-      System.out.println("You are running low on water! Play this number guessing game to replenish.")
+      System.out.println("You are running low on food and water! Play this number guessing game to replenish.")
       System.out.println("You have three guesses, pick any whole number between 1 & 20 inclusive."); 
       int guessNumber = (Math.random()*15)+5; 
       int guess = myObj.nextLine();
       for (int i = 0; i <= 3; i++){
 	      if (guessNumber == guess){
-		      numWater += 5; 
-		      return "Nice job! You guessed right! You have " + numWater + " water.";
+		      numWater += 3; 
+		      numFood += 3; 
+		      return "Nice job! You guessed right! You have " + numWater + " water and " + numFood + " food.";
 	      }
 	      else {
 		      System.out.println("Guess again."); 
 	      } 
       } 
-      return "Sorry! You are out of guesses, you currently have " + numWater + " water."; 
-      if (numWater <= 0) || (numFood <= 0){
-		return "You ran out of resources, you died.";
-	        break; 
-	    }
+      return "Sorry! You are out of guesses, you currently have " + numWater + " water and " + numFood + " food."; 
     } 
-		    
-    public String findFood(int guess){
-	    System.out.println("You are running low on food! Play this number guessing game to replenish.");
-	    System.out.println("You have three guesses, pick any letter between a-k (abcdefghijk)");
-	    String alphabet[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}; 
-    } 
-		    				       
+		    	    				       
     public boolean healthCheck(){
 	    if (numWater <= 0) || (numFood <= 0){
 		return false;
