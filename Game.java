574305f0e378d3b1fd70315243a37f7e0eb0cd0c
kashvi <79034048+Kashvi1666@users.noreply.github.com>
2022-06-02 10:37:33 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -8,7 +8,7 @@ public class Game{
     Scanner myObj = new Scanner(System.in);
     System.out.println("Please enter your username: ");
     String username = myObj.nextLine();
-    System.out.println("Welcome to our Island Game: " + username);
+    System.out.println("Welcome to our Island Game, " + username);
     System.out.println("In this game, you wil be faced with many obstacles and challenges that you will have to overcome. You will be given a random number of resources and have to make hard descisions to survive. If you live for 10 days, then you win! I wish you luck on your island adventure!");
     System.out.println("Below are the resources you have");
     Island2 island = new Island2(username);
